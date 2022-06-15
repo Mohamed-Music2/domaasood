0a9d3c7e45daadc2dcbbf90d7d73803d1fbde4d6
Mohamed-Music2 <105570732+Mohamed-Music2@users.noreply.github.com>
2022-06-15 16:41:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ from pytgcalls import PyTgCalls
 if os.path.exists(".env"):
     load_dotenv(".env")
 # الفارات
-API_ID = int(os.getenv("API_ID","16115488"))
-API_HASH = os.getenv("API_HASH","6d63bf20bc5c986c77332a98e5d65894")
-SESSION = os.getenv("SESSION","BAAmB3X6Lzhe8Q3J5VwB8WdBg8B6K3Hgz8em1cSWTO8OdYfpCp5zMMHHIF7cv0B7hOrC4Ik_tjJN5EAX5WfMtlOKU0ExcpbbBxF2iXhKvhuzPpTIf0ci5CEhOCeTWt1N1BaB3HK7LaN_O20YBa3h_5a5LI2qcLwQW2RMO9DuKS_m7HO6u2GFtK9m5GyBsr1gqEyPNyXjiOAHUmu-4G80ZJycmUYy44p2Jt6zMK82RCRoI1W737G3nAJ3iT4rjrVTp_m0aDL7JI1GnAXzSmrqMq40eI7-OsqN60IGSFCJBQ0lmPdb87LB5NA6Qtl1vaCjNjyCxH3eM7vQ46RRe-giRwnfAAAAATB36rAA")
-HNDLR = os.getenv("HNDLR", "")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS","5191100896").split()))
+API_ID = int(os.getenv("API_ID", "14446688"))
+API_HASH = os.getenv("API_HASH", "cb812098f053fe4b537e9e0ba4e1d806")
+SESSION = os.getenv("SESSION", "AgAA_pwfmU5iiIU-ORGj5ZqrW0wUb5iNdxE3lQipiXUQM5U4C-jVRIvkxg2JmJRzYObMqQM4dYWVRKaCP7VqqFIrhgi9S_wd490LB4eGw-gmuZylLSlM37GTacnGu43Fwj3C6BrhZYx7_cHTZfARG_nNyN8QDsn7fDj30pTKgpUvtOClsUNkWduzpsPZpSLJpDsTFXwNHYprXYU5vsyyOEaf_cznVruEWkzm8eTWj6sEYj30z8_Z4-q6g0qoYYDqhfodJw9KsPuCQuIXb8TFa7obpZuCEGFKYf6eyjW8tyz7Z46z36erNu-OROJOBMvV7oQ1jWSyuSda0bM84jA3JdNpAAAAATlXs_YA")
+HNDLR = os.getenv("HNDLR", "$")
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "5191100896").split()))
 contact_filter = filters.create(    lambda _, __, message: (message.from_user and message.from_user.is_contact)    or message.outgoing)
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicTelethon"))
 call_py = PyTgCalls(bot)
